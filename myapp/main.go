e009d13f410bd20db99b1c4be526f06eef9cc201
Sparrow535 <rohanrai4133@gmail.com>
2024-04-22 05:46:39 +0000

    first

--- myapp/main.go
+++ myapp/main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	route "myapp/routes"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	route.InitializeRoutes()
+	router := mux.NewRouter()
+
+	router.HandleFunc("/home", HomeHandler)
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		return
+	}
 }
 
-// Define handler function to handle incoming request
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Hello, World!"))
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+}
