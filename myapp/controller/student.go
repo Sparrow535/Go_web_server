a5e52f170560e8853d12bfe6edb6383b3047e5ec
Sparrow535 <rohanrai4133@gmail.com>
2024-04-23 18:08:10 +0000

    view

--- myapp/controller/student.go
+++ myapp/controller/student.go
@@ -1,9 +1,14 @@
 package controller
 
 import (
+	"database/sql"
 	"encoding/json"
 	"myapp/model"
+	"myapp/utils/httpResp"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func AddStudent(w http.ResponseWriter, r *http.Request) {
@@ -12,25 +17,103 @@ func AddStudent(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 
 	if err := decoder.Decode(&stud); err != nil {
-		response, _ := json.Marshal(map[string]string{"error": "invalid json body"})
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write(response)
+		httpResp.RespondWithError(w, http.StatusBadRequest, "Invalid json body")
 		return
 	}
 	defer r.Body.Close()
 
 	saveErr := stud.Create()
 	if saveErr != nil {
-		response, _ := json.Marshal(map[string]string{"error": saveErr.Error()})
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write(response)
+		httpResp.RespondWithError(w, http.StatusBadRequest, saveErr.Error())
 		return
 	}
 
-	response, _ := json.Marshal(map[string]string{"status": "student added"})
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	w.Write(response)
+	httpResp.RespondWithJSON(w, http.StatusCreated, map[string]string{"status": "student added"})
+}
+func GetStud(w http.ResponseWriter, r *http.Request) {
+	sid := mux.Vars(r)["sid"]
+	stdId, idErr := getUserId(sid)
+	if idErr != nil {
+		httpResp.RespondWithError(w, http.StatusBadRequest, idErr.Error())
+		return
+	}
+	s := model.Student{StdId: stdId}
+	getErr := s.Read()
+	if getErr != nil {
+		switch getErr {
+		case sql.ErrNoRows:
+			httpResp.RespondWithError(w, http.StatusNotFound, "Student not found")
+			return
+		default:
+			httpResp.RespondWithError(w, http.StatusInternalServerError, getErr.Error())
+		}
+		return
+	}
+	httpResp.RespondWithJSON(w, http.StatusOK, s)
+}
+
+func getUserId(userIdParam string) (int64, error) {
+	userId, userErr := strconv.ParseInt(userIdParam, 10, 64)
+	if userErr != nil {
+		return 0, userErr
+	}
+	return userId, nil
+}
+
+func UpdateStud(w http.ResponseWriter, r *http.Request) {
+	old_sid := mux.Vars(r)["sid"]
+	old_stdId, idErr := getUserId(old_sid)
+	if idErr != nil {
+		httpResp.RespondWithError(w, http.StatusBadRequest, idErr.Error())
+		return
+	}
+	var stud model.Student
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&stud); err != nil {
+		httpResp.RespondWithError(w, http.StatusBadRequest, "Invalid json body")
+		return
+	}
+	defer r.Body.Close()
+
+	updateErr := stud.Update(old_stdId)
+	if updateErr != nil {
+		switch updateErr {
+		case sql.ErrNoRows:
+			httpResp.RespondWithError(w, http.StatusNotFound, "Student not found")
+		default:
+			httpResp.RespondWithError(w, http.StatusInternalServerError, updateErr.Error())
+		}
+	} else {
+		httpResp.RespondWithJSON(w, http.StatusOK, stud)
+	}
+}
+
+func DeleteStud(w http.ResponseWriter, r *http.Request) {
+	sid := mux.Vars(r)["sid"]
+	stdId, idErr := getUserId(sid)
+	if idErr != nil {
+		httpResp.RespondWithError(w, http.StatusBadRequest, idErr.Error())
+		return
+	}
+	s := model.Student{StdId: stdId}
+	deleteErr := s.Delete()
+	if deleteErr != nil {
+		switch deleteErr {
+		case sql.ErrNoRows:
+			httpResp.RespondWithError(w, http.StatusNotFound, "Student not found")
+		default:
+			httpResp.RespondWithError(w, http.StatusInternalServerError, deleteErr.Error())
+		}
+		return
+	}
+	httpResp.RespondWithJSON(w, http.StatusOK, map[string]string{"status": "student deleted"})
+}
+
+func GetAllStuds(w http.ResponseWriter, r *http.Request) {
+	students, getErr := model.GetAllStudents()
+	if getErr != nil {
+		httpResp.RespondWithError(w, http.StatusBadRequest, getErr.Error())
+		return
+	}
+	httpResp.RespondWithJSON(w, http.StatusOK, students)
 }
--- myapp/routes/routes.go
+++ myapp/routes/routes.go
@@ -11,7 +11,24 @@ import (
 func InitializeRoutes() {
 	var port = 8080
 	router := mux.NewRouter()
+
+	//students routes
 	router.HandleFunc("/student", controller.AddStudent).Methods("POST")
+	router.HandleFunc("/student/{sid}", controller.GetStud).Methods("GET")
+	router.HandleFunc("/student/{sid}", controller.UpdateStud).Methods("PUT")
+	router.HandleFunc("/student/{sid}", controller.DeleteStud).Methods("DELETE")
+	router.HandleFunc("/students", controller.GetAllStuds)
+
+	//course routes
+	router.HandleFunc("/course", controller.AddCourse).Methods("POST")
+	router.HandleFunc("/course/{cid}", controller.GetCourse).Methods("GET")
+	router.HandleFunc("/course/{cid}", controller.UpdateCourse).Methods("PUT")
+	router.HandleFunc("/course/{cid}", controller.DeleteCourse).Methods("DELETE")
+	router.HandleFunc("/courses", controller.GetAllCourses)
+
+	fhandler := http.FileServer(http.Dir("./view"))
+	router.PathPrefix("/").Handler(fhandler)
+
 	log.Println("Application running on port", port)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
